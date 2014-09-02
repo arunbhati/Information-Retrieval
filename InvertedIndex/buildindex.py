4a83dc4b1c2612ba877c3d761f6dac0e66ec335b
Arsenal <sparshgunner12@gmail.com>
2014-09-02 18:20:25 +0530

    added document file

--- InvertedIndex/buildindex.py
+++ InvertedIndex/buildindex.py
@@ -9,7 +9,9 @@ ip = "172.16.27.30"
 user   = "root"
 password = "ir" 
 database = "information_retrieval"
-commit_iterations=100
+commit_iterations=1000
+document_len_file="doc_len.txt"
+
 class buildindex:
 	def __init__(self):
 		self.dict = {}
@@ -18,6 +20,8 @@ class buildindex:
 		self.databasecursor = self.connstring.cursor()
 		self.current_word_id=0
 		self.current_doc_id=0
+		self.fp_doc_len=open(document_len_file,"w")
+		
 
 	def insertdict(self,words):
 		current_word_index=1
@@ -39,20 +43,26 @@ class buildindex:
 				fw.write("Unable to process the query for " + str(self.current_doc_id) + " " + str(self.dict[a_word]) + " " + str(current_word_index) + " " + a_word + "\n")
 				current_word_index=current_word_index+1
 				fw.close()
+		self.fp_doc_len.write("%d %d\n"%(self.current_doc_id,current_word_index-1))
 			
 	def iteratefolder(self,filepath):
 		count=1
-		for dir_entry in os.listdir(filepath):
+		for i in range(10000):
+			dir_entry = str(i)
 			print self.current_doc_id
-			dir_entry_path = os.path.join(filepath, dir_entry)
+			dir_entry_path = filepath + dir_entry
 			if os.path.isfile(dir_entry_path):
 				if(self.current_doc_id%commit_iterations==0):
 					self.connstring.commit()
+					self.fp_doc_len.close()
+					self.fp_doc_len=open(document_len_file,"a")
 				htmlparser=	HtmlParser(filepath, dir_entry,False,True)
 				word_list = htmlparser.get_all_words()
 				self.insertdict(word_list)
 				self.current_doc_id=self.current_doc_id+1
-	
+			else:
+				print "Fatal Error , file not found %s"%(dir_entry_path)
+				sys.exit(-1)
 	
 
 if __name__ == '__main__':
