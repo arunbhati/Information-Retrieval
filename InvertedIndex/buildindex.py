3418f34a857c911aee41ab6a808bd1d92e1fb20b
arunbhati <arnbhati@gmail.com>
2014-09-02 23:53:57 +0530

    try to improve parser :)

--- InvertedIndex/buildindex.py
+++ InvertedIndex/buildindex.py
@@ -1,16 +1,20 @@
 import sys
 import MySQLdb
 import os
+import os.path
 from htmlparser import HtmlParser
+import pickle
+from os import listdir
+from os.path import isfile, join
 
 log_file = "log_file"
-######### Database 
 ip = "172.16.27.30"
 user   = "root"
 password = "ir" 
 database = "information_retrieval"
-commit_iterations=1000
+commit_iterations=200
 document_len_file="doc_len.txt"
+pickle_file_name="dictionary.pickle"
 
 class buildindex:
 	def __init__(self):
@@ -21,50 +25,57 @@ class buildindex:
 		self.current_word_id=0
 		self.current_doc_id=0
 		self.fp_doc_len=open(document_len_file,"w")
-		
 
 	def insertdict(self,words):
-		current_word_index=1
-		for a_word in words:
-			try:
-				if(self.dict.has_key(a_word)):
-					sql = "INSERT INTO posting_list (document_id,word_id,position) values(%d,%d,%d)"%(self.current_doc_id,self.dict[a_word],current_word_index)
-					self.databasecursor.execute(sql)
-					#print self.databasecursor.fetchall()
-				else:
-					self.dict[a_word]=self.current_word_id
-					self.current_word_id=self.current_word_id+1
-					sql = "INSERT INTO posting_list (document_id,word_id,position) values(%d,%d,%d)"%(self.current_doc_id,self.dict[a_word],current_word_index)
-					self.databasecursor.execute(sql)
-					#print self.databasecursor.fetchall()
-				current_word_index=current_word_index+1
-			except Exception as e:
-				fw = open(log_file,"a")
-				fw.write("Unable to process the query for " + str(self.current_doc_id) + " " + str(self.dict[a_word]) + " " + str(current_word_index) + " " + a_word + "\n")
-				current_word_index=current_word_index+1
-				fw.close()
-		self.fp_doc_len.write("%d %d\n"%(self.current_doc_id,current_word_index-1))
-			
+		
+		sql = "INSERT INTO posting_list (document_id,word_id,position) values "		
+		try:
+			for word in words:
+				if not self.dict.has_key(word):
+					self.dict[word] = self.current_word_id
+					self.current_word_id = self.current_word_id + 1
+
+			rsql = sql + ','.join(["(%d,%d,%d)"%(self.current_doc_id,self.dict[words[index]],index) for index in xrange(len(words)) ])
+			if rsql != sql:			
+				self.databasecursor.execute(rsql)
+		except Exception as e:
+			fw = open(log_file,"a")
+			fw.write("Unable to process the query for " + str(self.current_doc_id) + "\n")
+			fw.close()
+		self.fp_doc_len.write("%d %d\n"%(self.current_doc_id,len(words)))
+
 	def iteratefolder(self,filepath):
-		count=1
-		for i in range(10000):
-			dir_entry = str(i)
-			print self.current_doc_id
-			dir_entry_path = filepath + dir_entry
-			if os.path.isfile(dir_entry_path):
-				if(self.current_doc_id%commit_iterations==0):
-					self.connstring.commit()
-					self.fp_doc_len.close()
-					self.fp_doc_len=open(document_len_file,"a")
-				htmlparser=	HtmlParser(filepath, dir_entry,False,True)
-				word_list = htmlparser.get_all_words()
-				self.insertdict(word_list)
-				self.current_doc_id=self.current_doc_id+1
-			else:
-				print "Fatal Error , file not found %s"%(dir_entry_path)
-				sys.exit(-1)
-	
+		count = 1
+		
+		document_list = [ document_name for document_name in listdir(filepath) if isfile(join(filepath,document_name)) ]
+
+		for document in document_list:
+			if document.isdigit():
+				print document
+				dir_entry_path = filepath + document
+				if os.path.isfile(dir_entry_path):
+					if self.current_doc_id%commit_iterations==0 :
+						self.connstring.commit()
+						self.fp_doc_len.close()
+						self.fp_doc_len=open(document_len_file,"a")
+					htmlparser=	HtmlParser(filepath, document,False,True)
+					word_list = htmlparser.get_all_words()
+					self.insertdict(word_list)
+					self.current_doc_id= int(document)
+				else:
+					print "Fatal Error , file not found %s"%(dir_entry_path)
+					sys.exit(-1)
+
 
 if __name__ == '__main__':
 	a=buildindex()
-	a.iteratefolder(sys.argv[1])
+	
+	directory_name  = sys.argv[1]
+	folder_list = [os.path.join(directory_name,o) for o in os.listdir(directory_name) if os.path.isdir(os.path.join(directory_name,o))]
+
+	for folder in folder_list:
+		a.iteratefolder(folder+"/")
+
+	fp = open(pickle_file_name,"wb")
+	pickle.dump(a.dict,fp)
+	fp.close()	
--- InvertedIndex/main.py
+++ InvertedIndex/main.py
@@ -2,11 +2,11 @@ from htmlparser import HtmlParser
 
 def fetch_data():
    # fileName = raw_input("Enter File Name : ")
-    htmlParser = HtmlParser("/home/arsenal/IR/0/", "0",False,True)
-
-    word_list = htmlParser.get_all_words()
-    for word in word_list:
-        print word
+    
+	htmlParser = HtmlParser("/home/arun/0/", "0",False,True)
+	word_list = htmlParser.get_all_words()
+	for word in word_list:
+		print word
 
 if __name__ == '__main__':
     fetch_data()
